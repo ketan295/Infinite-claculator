99b4b6a850ff756cb6c440ab8d9d0795b0ce92c8
ketan295 <130463504+ketan295@users.noreply.github.com>
2023-05-11 18:49:16 +0530

    Add files via upload

---
+++ Calculator.c
@@ -0,0 +1,96 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int add(a,b)
+{
+	return a+b;
+}
+
+
+int sub(a,b)
+{
+	return a-b;
+}
+
+int mul(a,b)
+{
+	return a*b;
+}
+
+int divi(a,b)
+{
+	return a/b;
+}
+
+int mod(a,b)
+{
+	return a%b;
+}
+
+
+main()
+{
+	int a,b;
+	int choice;
+
+	printf("0->Exit\n");
+	printf("1->Addition\n");
+	printf("2->Substraction\n");
+	printf("3->Multiplication\n");
+	printf("4->Division\n");
+	printf("5->modulas\n");
+	
+
+   do
+   {
+			 
+	    printf("\nEnter value of a: ");
+        scanf("%d",&a);
+    	printf("\nEnter value of b: ");
+        scanf("%d",&b);
+     	
+		printf("\nEnter your choice:");
+		scanf("%d",&choice);
+		
+   switch(choice)
+   {
+	   
+	   case 0 :
+	   {
+	   	exit(0);
+	   }
+	   
+		
+		case 1:
+			{
+				printf("a+b=%d",add(a,b));
+				break;
+			}
+		case 2:
+		    {
+        		printf("a-b=%d",sub(a,b));
+		    	break;
+			}
+		case 3:
+			{
+				printf("a*b=%d",mul(a,b));
+				break;
+			}
+		case 4:
+			{
+				printf("a/b=%d",divi(a,b));
+				break;
+			}
+		case 5:
+			{
+				printf("a%%b=%d",mod(a,b));
+				break;
+			}
+		default :
+			{
+				printf("Please enter correct value!!");
+			}
+	   }
+	}
+	while(choice!=0);
+}
